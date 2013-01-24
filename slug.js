1ef3b0ad2e1ea43847a2fbd0cc960543d91ba5f7
Chris McBride <chris@vistarmedia.com>
2013-01-24 16:12:10 -0500

    Fix travis tests
    Change-Id: I2ed1a153e0579c4d4af554f35774b113170141a6

--- slug.js
+++ slug.js
@@ -1,9 +1,3 @@
-var fs   = require('fs-extra');
 var argv = process.argv.slice(2);
-fs.copy('./css/jqueryui-smoothness/images', './public/images', function(err){
-  if (err) {
-    console.error(err);
-  }
-});
 var hem = require('hem-haml-coffee');
 hem.exec(argv[0]);
